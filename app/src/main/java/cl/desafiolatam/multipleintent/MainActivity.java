ea6bf1bfc4171893adaa9f25168df9f55c911210
Patricio García A <pgarcia@serlitoral.cl>
2020-11-17 11:32:15 -0300

    Carga de imagen en ActivityMain

--- app/src/main/java/cl/desafiolatam/multipleintent/MainActivity.java
+++ app/src/main/java/cl/desafiolatam/multipleintent/MainActivity.java
@@ -1,14 +1,53 @@
 package cl.desafiolatam.multipleintent;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imagenView;
+    Button cargarImagenBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imagenView = findViewById(R.id.imagen_view);
+        cargarImagenBtn = findViewById(R.id.btnCargarImagen);
+
+        //Botón Cargar Imagen
+        cargarImagenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tomarFoto();
+            }
+        });
+    }
+
+    private void tomarFoto() {
+        Intent fotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (fotoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(fotoIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imagenBitmap = (Bitmap) extras.get("data");
+            imagenView.setImageBitmap(imagenBitmap);
+        }
     }
 }
---
+++ app/src/main/java/cl/desafiolatam/multipleintent/ResultActivity.java
@@ -0,0 +1,4 @@
+package cl.desafiolatam.multipleintent;
+
+public class ResultActivity {
+}
